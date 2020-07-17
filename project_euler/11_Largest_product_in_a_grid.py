aefceb777eb3e5f2f935ce0dbe63731183420a77
Yam Tirosh <yam.tirosh@gmail.com>
2020-07-17 16:34:27 +0300

    Merge 11: Largest product in a grid (#3)

---
+++ project_euler/11_Largest_product_in_a_grid.py
@@ -0,0 +1,133 @@
+"""
+https://projecteuler.net/problem=11
+
+Largest product in a grid
+Problem 11
+
+In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+
+The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+
+What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+"""
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+SET_SIZE = 4
+GRID = """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"""
+
+Grid = List[List[int]]
+
+
+def extract_grid(grid_str: str = GRID) -> Grid:
+    result = []
+    for i, line in enumerate(grid_str.split("\n")):
+        result.append([])
+        for num in line.split():
+            result[i].append(int(num))
+    return result
+
+
+class Direction(str, Enum):
+    RIGHT = "right"
+    DOWN = "down"
+    MAIN_DIAGONAL = "right and down"
+    SECONDARY_DIAGONAL = "left and down"
+
+    def get_deltas(self) -> (int, int):
+        """
+        Get row and column deltas for the direction.
+        :return (row delta, column delta)
+        """
+        deltas = {
+            self.RIGHT: (0, 1),
+            self.DOWN: (1, 0),
+            self.MAIN_DIAGONAL: (1, 1),
+            self.SECONDARY_DIAGONAL: (1, -1),
+        }
+
+        return deltas[self.value]
+
+
+@dataclass
+class Set:
+    position: (int, int)
+    direction: Direction
+    size: int = SET_SIZE
+
+    def __iter__(self):
+        row_delta, column_delta = self.direction.get_deltas()
+        for i in range(self.size):
+            yield self.position[0] + (row_delta * i), self.position[1] + (column_delta * i)
+
+
+def find_largest_product(grid: Grid) -> (int, Set):
+    largest_product, result_set = 0, None
+
+    for i, line in enumerate(grid):
+        for j, num in enumerate(line):
+            for direction in Direction:
+                product = 1
+                current_set = Set(position=(i, j), direction=Direction(direction))
+
+                for row, col in current_set:
+                    if not (0 <= row < len(grid) and 0 <= col < len(grid[0])):
+                        break
+                    product *= grid[row][col]
+                else:
+                    if product > largest_product:
+                        largest_product, result_set = product, current_set
+
+    return largest_product, result_set
+
+
+def main():
+    grid = extract_grid()
+    largest_product, result_set = find_largest_product(grid)
+    print(f"Largest product {largest_product} in {result_set.position} to the {result_set.direction}")
+    print(f"Set: {[grid[row][col] for row, col in result_set]}")
+
+
+if __name__ == '__main__':
+    main()
